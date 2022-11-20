5d7e8c7426dfc5bed1fb8730dc55d6f6326de9f7
Jinny <ppwm111@naver.com>
2022-11-20 18:13:17 +0900

    feat: 맵 리스트 뿌려보기 #14

--- src/apis/ship.js
+++ src/apis/ship.js
@@ -85,3 +85,20 @@ export const detailFishingList = async (id, page) => {
     console.log(error);
   }
 };
+
+// -------------------------선박 리스트 ---------------------
+export const shipList = async (
+  { page, searchRequirements, sortBy, sortMethod },
+  content,
+) => {
+  console.log({ page, searchRequirements, sortBy, sortMethod }, content);
+  try {
+    const res = await axios.get(
+      `/api/ship/list?page=${page}&searchRequirements=${searchRequirements}&searchWord=${content}&sortBy=${sortBy}&sortMethod=${sortMethod}`,
+    );
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
